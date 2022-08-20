52ba69ee7c8e3948d8999ddd9e01115bd72d44a8
João Gustavo Bispo <jgbispo20@gmail.com>
2022-08-20 00:21:22 -0300

    New Component
    Added transaction table, still manually

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,104 @@
 import {
-  Box, HStack, VStack,
-} from "@chakra-ui/react"
-import Card from "./components/Card"
-import Header from "./components/Header"
-import SearchBar from "./components/SearchBar"
+  Box,
+  Table,
+  TableContainer,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  HStack,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import Card from "./components/Card";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import TableItem from "./components/TableItem";
+interface Transation {
+  id: number;
+  name: string;
+  value: number;
+  desc: string;
+  type: Boolean;
+  data: string;
+}
 
 function App() {
+  const [transactions, setTransactions] = useState<Transation[]>([
+    {
+      id: 1,
+      name: "Desenvolvimento de site",
+      value: 1200,
+      desc: 'Serviço',
+      type: true,
+      data: "29/08/2022",
+    },
+    {
+      id: 2,
+      name: "Aluguel",
+      value: 2200,
+      desc: 'Serviço',
+      type: false,
+      data: "29/08/2022",
+    },
+  ]);
+
+  function SetItem(item: Transation) {
+    setTransactions([...transactions, item]);
+  }
+
+  const RemoveItem = (id: number) => {
+    const newListTransactions = transactions.filter(item => item.id !== id);
+    setTransactions(newListTransactions)
+  }
+
   return (
-    <Box bgGradient='linear(to-b, #121214 20%, #202024 20%)' h='100vh' w='100vw' flex='1'>
+    <Box
+      bgGradient="linear(to-b, #121214 30%, #202024 30%)"
+      h="100vh"
+      w="100vw"
+    >
       <VStack>
         <Header />
-        <HStack justify='center' pt='40px' spacing='32px'>
+        <HStack justify="center" pt="40px" spacing="32px">
           <Card type="Income" value={520} />
           <Card type="Expense" value={220} />
           <Card type="Money" value={300} />
         </HStack>
         <SearchBar />
+        <TableContainer>
+          <Table size="md" w="82vw" variant="simple">
+            <Thead>
+              <Tr>
+                <Th >Name</Th>
+                <Th isNumeric>Value</Th>
+                <Th >Description</Th>
+                <Th >Data</Th>
+                <Th></Th>
+                <Th></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {transactions.map((item, index) => {
+                return (
+                  <TableItem
+                    key={index}
+                    id={item.id}
+                    name={item.name}
+                    desc={item.desc}
+                    type={item.type}
+                    data={item.data}
+                    value={item.value}
+                    del={RemoveItem}
+                  />
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
       </VStack>
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TableItem.tsx
@@ -0,0 +1,47 @@
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import { IconButton, Stack, Td, Tr } from "@chakra-ui/react";
+
+interface Transaction {
+  id: number;
+  name: string;
+  value: number;
+  desc: string;
+  type: Boolean;
+  data: string;
+  del: Function;
+}
+
+export default function TableItem(props: Transaction) {
+  return (
+    <Tr>
+      <Td>{props.name}</Td>
+      <Td isNumeric color={props.type ? "green" : "red"}>
+        {props.type ? `R$ ${props.value}` : `- R$ ${props.value}`}
+      </Td>
+      <Td>{props.desc}</Td>
+      <Td>{props.data}</Td>
+        <Td isNumeric>
+          <IconButton
+            aria-label="edit"
+            variant="outline"
+            colorScheme="blue"
+            icon={<EditIcon />}
+            onClick={() => {
+              props.del(props.id);
+            }}
+          />
+        </Td>
+        <Td>
+          <IconButton
+            aria-label="delete"
+            variant="outline"
+            colorScheme="red"
+            icon={<DeleteIcon />}
+            onClick={() => {
+              props.del(props.id);
+            }}
+          />
+        </Td>
+    </Tr>
+  );
+}
